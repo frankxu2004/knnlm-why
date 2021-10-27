06eaa97572c3288a054f308f190d9408bc1a8581
Frank Xu <frankxu2004@gmail.com>
2021-10-27 12:34:19 -0400

    kmeans

--- cluster/extract_vocab_vecs.py
+++ cluster/extract_vocab_vecs.py
@@ -9,20 +9,25 @@ vec_dim = 1024
 dictionary = Dictionary.load('data-bin/wikitext103-bpe/dict.txt')
 print(len(dictionary))
 
-keys_from_memmap = np.memmap('checkpoints/wikitext103-bpe/dstore_keys.npy',
-                             dtype=np.float16, mode='r', shape=(dstore_size, vec_dim))
 vals_from_memmap = np.memmap('checkpoints/wikitext103-bpe/dstore_vals.npy',
                              dtype=np.int, mode='r', shape=(dstore_size, 1))
 
-
-keys = np.zeros((dstore_size, vec_dim), dtype=np.float16)
 vals = np.zeros((dstore_size, 1), dtype=np.int)
 
-keys[:] = keys_from_memmap[:]
 vals[:] = vals_from_memmap[:]
+vals = vals.squeeze()
+
+first_zero_idx = (vals_from_memmap == 0).argmax(axis=0)
+
+keys_from_memmap = np.memmap('checkpoints/wikitext103-bpe/dstore_keys.npy',
+                             dtype=np.float16, mode='r', shape=(dstore_size, vec_dim))
+
+keys = np.zeros((dstore_size, vec_dim), dtype=np.float16)
+keys[:] = keys_from_memmap[:]
+
 del keys_from_memmap, vals_from_memmap
 
 vals = vals.squeeze()
 
-to_cluster = keys[vals>=dictionary.nspecial]
+to_cluster = keys[vals >= dictionary.nspecial]
 np.save('checkpoints/wikitext103-bpe/to_cluster.npy', to_cluster)
--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -14,15 +14,28 @@ print(len(dictionary))
 
 keys = np.memmap('checkpoints/wikitext103-bpe/dstore_keys.npy',
                              dtype=np.float16, mode='r', shape=(dstore_size, vec_dim))
-vals = np.memmap('checkpoints/wikitext103-bpe/dstore_vals.npy',
+vals_from_memmap = np.memmap('checkpoints/wikitext103-bpe/dstore_vals.npy',
                              dtype=np.int, mode='r', shape=(dstore_size, 1))
+
+vals = np.zeros((dstore_size, 1), dtype=np.int)
+
+vals[:] = vals_from_memmap[:]
+del vals_from_memmap
 
 vals = vals.squeeze()
 
-to_cluster = keys[vals>=dictionary.nspecial]
-np.save('checkpoints/wikitext103-bpe/to_cluster.npy', to_cluster)
+first_zero_idx = (vals == 0).argmax(axis=0)
+print(first_zero_idx)
+to_cluster = np.zeros((first_zero_idx, vec_dim), dtype=np.float16)
+to_cluster[:] = keys[:first_zero_idx]
+
+# subsample for training kmeans
+rs = np.random.RandomState(1)
+idx = rs.choice(np.arange(first_zero_idx), size=int(0.2*first_zero_idx), replace=False)
+to_cluster = to_cluster[idx]
 to_cluster = to_cluster.astype(np.float32)
 
+print('start cluster')
 ncentroids = dictionary.nspecial + ratio * (len(dictionary) - dictionary.nspecial)
 niter = 20
 verbose = True
@@ -32,7 +45,7 @@ kmeans.train(to_cluster)
 
 np.save('checkpoints/wikitext103-bpe/centroids.npy', kmeans.centroids)
 
-D, I = kmeans.index.search(to_cluster, 1)
+D, I = kmeans.index.search(keys[:first_zero_idx], 1)
 
 np.save('checkpoints/wikitext103-bpe/dist.npy', D)
 np.save('checkpoints/wikitext103-bpe/centroid_ids.npy', I)
