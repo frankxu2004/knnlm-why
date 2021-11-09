d2f210d81ac64724ea75c2bcf85717f5d3059f2a
Frank Xu <frankxu2004@gmail.com>
2021-11-09 04:58:00 -0500

    update

--- cluster/check_weight_norms.py
+++ cluster/check_weight_norms.py
@@ -2,20 +2,29 @@ import numpy as np
 from fairseq.data import Dictionary
 import matplotlib.pyplot as plt
 
+# ckpt_path = 'checkpoints/wikitext103-bpe/'
+ckpt_path = 'checkpoints/wikitext103-bpe/last_linear_inp/'
+
 dictionary = Dictionary.load('data-bin/wikitext103-bpe/dict.txt')
 
 weight_matrix = np.load('checkpoints/wikitext103-bpe/out_embed.npy')
 
-token_centroids = np.load('checkpoints/wikitext103-bpe/centroid_word.npy')
+token_centroids = np.load(ckpt_path + 'centroid_word.npy')
 
-kmeans_centroids = np.load('checkpoints/wikitext103-bpe/centroids.npy')
+kmeans_centroids = np.load(ckpt_path + 'centroids.npy')
 
 out_emb_norms = np.linalg.norm(weight_matrix, axis=1)
 token_centroids_norms = np.linalg.norm(token_centroids, axis=1)
 kmeans_centroids_norms = np.linalg.norm(kmeans_centroids, axis=1)
 
-bins = np.linspace(0, 7, 100)
+print(token_centroids_norms.mean())
+print(kmeans_centroids_norms.mean())
+print(out_emb_norms.mean())
 
-plt.hist([out_emb_norms, token_centroids_norms, kmeans_centroids_norms], bins=bins, label=['emb', 'centroid', 'kmeans'], density=True)
+bins = np.linspace(0, 75, 100)
+
+plt.hist([out_emb_norms, token_centroids_norms, kmeans_centroids_norms],
+         bins=bins, label=['emb', 'centroid', 'kmeans'], density=True)
 plt.legend(loc='upper right')
-plt.savefig('embedding_norms.png')
+plt.title('last_linear_input')
+plt.savefig('embedding_norms_last_linear_input.png')
--- wikitext_bpe_load_centroids.sh
+++ wikitext_bpe_load_centroids.sh
@@ -21,7 +21,6 @@ python eval_lm.py data-bin/wikitext103-bpe \
 
 
 # last_linear_input
-
 ## eval - kmeans
 python eval_lm.py data-bin/wikitext103-bpe \
     --path checkpoints/wikitext103-bpe/checkpoint_best.pt \
@@ -31,3 +30,12 @@ python eval_lm.py data-bin/wikitext103-bpe \
     --model-overrides "{'use_last_ffn_input': False}" \
     --load-centroids checkpoints/wikitext103-bpe/last_linear_inp/centroids.npy \
     --load-centroid-distribution checkpoints/wikitext103-bpe/last_linear_inp/cluster_freq.npz
+
+## eval - word centroid
+python eval_lm.py data-bin/wikitext103-bpe \
+    --path checkpoints/wikitext103-bpe/checkpoint_best.pt \
+    --sample-break-mode complete --max-tokens 3072 \
+    --context-window 2560 --softmax-batch 1024 \
+    --gen-subset valid --bpe subword_nmt --remove-bpe \
+    --model-overrides "{'use_last_ffn_input': False}" \
+    --load-centroids checkpoints/wikitext103-bpe/last_linear_inp/centroid_word.npy
