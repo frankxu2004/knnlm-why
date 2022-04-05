c13ca69461a585b9e830e063601ee8eb78ff9fc3
Frank Xu <frankxu2004@gmail.com>
2022-04-05 10:41:33 -0400

    update

--- wikitext_bpe.sh
+++ wikitext_bpe.sh
@@ -28,9 +28,26 @@ python train.py --task language_modeling \
   --max-tokens 3072 --update-freq 3 --tokens-per-sample 3072 --seed 1 \
   --sample-break-mode none --skip-invalid-size-inputs-valid-test --ddp-backend=no_c10d --fp16
 
+# non tied change seed
+python train.py --task language_modeling \
+    data-bin/wikitext103-bpe \
+  --save-dir checkpoints/wikitext103-bpe \
+  --arch transformer_lm_wikibpe \
+  --max-update 286000 --max-lr 1.0 --t-mult 2 --lr-period-updates 270000 --lr-scheduler cosine --lr-shrink 0.75 \
+  --warmup-updates 16000 --warmup-init-lr 1e-07 --min-lr 1e-09 --optimizer nag --lr 0.0001 --clip-norm 0.1 \
+  --max-tokens 3072 --update-freq 3 --tokens-per-sample 3072 --seed 3 \
+  --sample-break-mode none --skip-invalid-size-inputs-valid-test --ddp-backend=no_c10d --fp16
+
 ## eval
 python eval_lm.py data-bin/wikitext103-bpe \
     --path checkpoints/wikitext103-bpe/checkpoint_best.pt \
+    --sample-break-mode complete --max-tokens 3072 \
+    --context-window 2560 --softmax-batch 1024 \
+    --gen-subset valid --bpe subword_nmt --remove-bpe
+
+## eval seed3
+python eval_lm.py data-bin/wikitext103-bpe \
+    --path checkpoints/wikitext103-bpe-seed3/checkpoint_best.pt \
     --sample-break-mode complete --max-tokens 3072 \
     --context-window 2560 --softmax-batch 1024 \
     --gen-subset valid --bpe subword_nmt --remove-bpe
